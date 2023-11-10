e0145b9919ccc9fa668d7f247c8724129a6b24a3
Hadiyo11 <hadyhope@gmail.com>
2023-11-10 07:08:25 +0200

    chapter04

---
+++ Classwork/Chapter04/tryityourself.sql
@@ -0,0 +1,20 @@
+CREATE TABLE movies(
+    id numeric,
+    movie varchar(90),
+    actor varchar(90)
+);
+
+INSERT INTO movies
+VALUES
+    (50, 'Mission Impossible', 'Tom Cruise');
+
+COPY movies (id, movie, actor)
+FROM 'C:\SQL\movies.txt'
+WITH (FORMAT CSV, HEADER);
+
+COPY (
+  SELECT county_name, state, housing_unit_count_100_percent
+  FROM us_counties_2010
+  ORDER BY housing_unit_count_100_percent DESC
+  LIMIT 20
+) TO 'C:\SQL\output.csv' CSV HEADER;
